46427f5f3c0ef7c5e749e56ff1c85a6d5ce1ff07
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2024-01-01 16:34:21 -0600

    Agregamos el NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,39 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <h1>Hola desde NavBar</h1>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">NDG</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Plantilla 1</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Plantilla 2</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Plantilla 3</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Contacto</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Servicios
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Reparaciones</a></li>
+                                <li><a class="dropdown-item" href="#">Ventas</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
 
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import NavBar from './NavBar/NavBar';
 
 const App = () => {
     return (
-        <h1>Hola desde app</h1>
+        <NavBar />
     );
 };
 
