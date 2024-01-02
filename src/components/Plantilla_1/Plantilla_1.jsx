e369b60b5f26c57a18bb56a1d5322f900694df3c
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2024-01-01 19:16:26 -0600

    Priemras partes de la plantilla 1

--- src/components/Plantilla_1/Plantilla_1.jsx
+++ src/components/Plantilla_1/Plantilla_1.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+// Importamos la imagen
+import imagenTest_1 from '../../Imagenes/Plantilla1_test.jpg'
 
 const Plantilla_1 = () => {
     return (
-        <h1>Hola desde Plantilla_1</h1>
+        <>
+            <article className='mt-5 mb-5'>
+                <div className="card mb-3 border-0 mb-5">
+                    <div className="row g-0 justify-content-center">
+                        <div className="col-md-6 row align-items-center">
+                            <div className="card-body">
+                                <h2 className="card-title">NDG, somos la mejor opción en reparaciones de equipo de dental.</h2>
+                                <p className="card-text">En este espacio podemos mostrar información de la empresa, ya sea experiencia en el
+                                    mercado, clientes con los que se ha trabjado, etc.</p>
+                                <a href="#" class="btn btn-dark">Contactanos</a>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <img src={imagenTest_1} className="img-fluid object-fit-cover rounded" alt="..." />
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </>
     );
 };
 
