8029d5734bb74c22cb54570ae83bc4ea62327687
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-01-06 13:51:14 -0600

    Espaciado entre main y Navbar

--- src/components/Plantilla_1/Plantilla_1.jsx
+++ src/components/Plantilla_1/Plantilla_1.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 // Importamos la imagen
-import imagenTest_1 from '../../Imagenes/Plantilla1_test.jpg'
+import imagenTest_1 from '../../Imagenes/Plantilla1_test.jpg';
+// Importamos estilos
+import '../../estilos/main_Plantilla1.css';
 
 const Plantilla_1 = () => {
     return (
-        <>
-            <article className='mt-5 mb-5'>
-                <div className="card mb-3 border-0 mb-5">
-                    <div className="row g-0 justify-content-center">
-                        <div className="col-md-6 row align-items-center">
-                            <div className="card-body">
-                                <h2 className="card-title">NDG, somos la mejor opción en reparaciones de equipo de dental.</h2>
-                                <p className="card-text">En este espacio podemos mostrar información de la empresa, ya sea experiencia en el
-                                    mercado, clientes con los que se ha trabjado, etc.</p>
-                                <a href="#" class="btn btn-dark">Contactanos</a>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <img src={imagenTest_1} className="img-fluid object-fit-cover rounded" alt="..." />
+        <main className='mainP1'>
+            <div className="card mb-3 border-0 mb-5">
+                <div className="row g-0 justify-content-center">
+                    <div className="col-md-6 row align-items-center">
+                        <div className="card-body">
+                            <h2 className="card-title">NDG, somos la mejor opción en reparaciones de equipo de dental.</h2>
+                            <p className="card-text">En este espacio podemos mostrar información de la empresa, ya sea experiencia en el
+                                mercado, clientes con los que se ha trabjado, etc.</p>
+                            <a href="#" class="btn btn-dark">Contactanos</a>
                         </div>
                     </div>
+                    <div className="col-md-4">
+                        <img src={imagenTest_1} className="img-fluid object-fit-cover rounded" alt="..." />
+                    </div>
                 </div>
-            </article>
-        </>
+            </div>
+        </main>
     );
 };
 
