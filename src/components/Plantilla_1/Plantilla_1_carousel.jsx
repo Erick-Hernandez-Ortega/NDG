c571b04b7043b286579b8c5adf4fc0a4def75970
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-01-07 21:57:10 -0600

    últimos cambios a la plantilla 1

--- src/components/Plantilla_1/Plantilla_1_carousel.jsx
+++ src/components/Plantilla_1/Plantilla_1_carousel.jsx
@@ -7,37 +7,45 @@ import './styles/carousel_P1.css';
 const Plantilla_1_carousel = () => {
     return (
         <section className='section-servicios'>
-            <h2 className="text-center">Servicios</h2>
-            <div className="row row-cols-1 row-cols-md-3 g-4 mt-5 justify-content-evenly">
-                <div className="col">
+            <h2 className="text-center mb-5">Servicios</h2>
+
+            <div className="row row-cols-2 g-4 justify-content-evenly mt-5">
+                <div className="col" style={{ maxWidth: '35%' }}>
                     <div className="card">
                         <img src={imagenTest_1} className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h4 className="card-title">Reparaciones</h4>
-                            <p className="card-text">En esta seccion se podrían mencionar algo de las reparaciones y hacer que cuando le den clic
-                                te mande a la sección de reaparaciones donde se muestran los porductos que reparan</p>
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         </div>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col" style={{ maxWidth: '35%' }}>
                     <div className="card">
                         <img src={imagenTest_1} className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h4 className="card-title">Venta</h4>
-                            <p className="card-text">Esta sección igual que la anterior pero mostrando los prodcutos que venden</p>
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         </div>
                     </div>
                 </div>
-                {/*                 <div className="col">
+                <div className="col" style={{ maxWidth: '35%' }}>
                     <div className="card">
                         <img src={imagenTest_1} className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h4 className="card-title">Contacto</h4>
-                            <p className="card-text">Y en esta sección tal vez algo sobre el contacto e igual, cuando el den clic se puede redireccionar
-                                a un formulario de contacto para que les manden correo o mensaje</p>
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
                         </div>
                     </div>
-                </div> */}
+                </div>
+                <div className="col" style={{ maxWidth: '35%' }}>
+                    <div className="card">
+                        <img src={imagenTest_1} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
