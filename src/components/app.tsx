e44e8c5b0976130b08103b61849df1d9a4accb15
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-01-09 21:46:14 -0600

    Cambio en las rutas para que jale

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,8 +1,8 @@
 import React, { ReactElement } from 'react';
-import NavBar from './NavBar/NavBar';
+import NavBar from './NavBar/NavBar.tsx'
 import Plantilla_1 from './Plantilla_1/Plantilla_1';
 import Plantilla_1_carousel from './Plantilla_1/Plantilla_1_carousel';
-import Plantilla_2 from './Plantilla_2/Plantilla_2';
+import Plantilla_2 from './Plantilla_2/Plantilla_2.tsx';
 import Plantilla_3 from './Plantilla_3/Plantilla_3';
 import { Routes, Route } from 'react-router-dom';
 
